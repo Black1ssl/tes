c10e3d71dcd929c42c417c5f775ef6d5d411bfaf
Wirlian Alfathin <106651413+Black1ssl@users.noreply.github.com>
2025-01-23 11:24:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Wir\n' // Nama kamu
+            + 'ORG:bot1.1;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6285191972497:+62 851-9197-2497\n' // Nomor whatsapp kamu.6285191972497
             + 'END:VCARD'
 const
 {
